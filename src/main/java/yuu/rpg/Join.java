22d605b21907a8331c9345d3a709b98122491c61
yuu111 <yuu210yuu@gmail.com>
2018-12-19 14:47:40 +0900

    初参加時にymlにUUIDなどを書き込むように

--- src/main/java/yuu/rpg/Join.java
+++ src/main/java/yuu/rpg/Join.java
@@ -3,6 +3,7 @@ package yuu.rpg;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
@@ -13,20 +14,23 @@ public class Join  implements Listener {
     private RPG plugin;
     CustomConfig uuid;
 
-     Join(RPG pl) {
+    Join(RPG pl) {
         plugin = pl;
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerJoin(PlayerJoinEvent e) {
 
         uuid = new CustomConfig(plugin, "UUID.yml");
         FileConfiguration config = uuid.getConfig();
         Player p = e.getPlayer();
         UUID id = p.getUniqueId();
-        p.sendMessage("test");
-        if(config.getString("UUID."+ id +".Job") == "") {
+        String Job = config.getString("UUID." + id + ".Job");
+        p.sendMessage(Job);
+        if (Job == null) {
             config.set("UUID." + id + ".Job", "none");
+            uuid.saveConfig();
         }
     }
 }
--- src/main/java/yuu/rpg/RPG.java
+++ src/main/java/yuu/rpg/RPG.java
@@ -13,7 +13,7 @@ public final class RPG extends JavaPlugin {
         saveDefaultConfig();
         new CustomConfig(this, "UUID.yml").saveDefaultConfig();
 
-        getServer().getPluginManager().registerEvents(new Join(this), this);
+        new Join(this);
         getServer().getPluginManager().registerEvents(new DeathRespawn(), this);
         getCommand("job").setExecutor(new JobCommand(this));
     }
